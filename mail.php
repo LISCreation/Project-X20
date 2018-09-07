7d17d70aa56525e59f64c7fad28a8587e6674c7b
Alexander <lis.lightinsky@gmail.com>
2018-09-07 17:02:28 +0300

    Fix form
    Add conditions

--- mail.php
+++ mail.php
@@ -3,13 +3,22 @@ define('DATABASE_USER', 'LIGHTIX');
 define('DATABASE_PASS', 'hello.bear28');
 $login = $_POST['login'];
 $pass = $_POST['pass'];
-
-if(isset($login) && isset($pass)) {
-	if($login == DATABASE_USER && $pass == DATABASE_PASS) : echo "Доступ разрешён";
-} else if(empty($pass)) {
-	echo "Введите пароль";
-} else if(empty($login)) {
-	echo "Введите ник";
-} else {
-	echo "Недостаточно данных! Введите логин и пароль";
-}
+if(isset($login) && isset($login)) :
+	if(empty($login) && empty($pass)) {
+		echo "Пусто";
+	}
+	else if(empty($login)) {
+		echo "Не введён ник";
+	} else if(empty($pass)) {
+		echo "Не введён пароль";
+	} else {
+		if($login == DATABASE_USER && $pass == DATABASE_PASS) echo "Доступ разрешён";
+		else echo "Доступ запрещён";
+	}
+else :
+	header("Location: index.html");
+endif;
+?>
+<pre>
+	<?php print_r($GLOBALS);?>
+</pre>
